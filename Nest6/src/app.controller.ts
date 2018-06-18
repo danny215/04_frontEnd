2fc0347dd9df6da196c5f2a119a9e169a15548aa
danny215 <dannironmask@gmail.com>
2018-06-18 11:00:20 -0500

    practicas
    rtgr

---
+++ Nest6/src/app.controller.ts
@@ -0,0 +1,22 @@
+import {Get, Controller, UseGuards, ReflectMetadata} from '@nestjs/common';
+import {AppService} from './app.service';
+import {JwtGuard} from "./guards/jwt.guard";
+
+@Controller()
+@UseGuards(JwtGuard)
+export class AppController {
+    constructor(private readonly appService: AppService) {
+    }
+
+    @Get()
+    @ReflectMetadata("nesecitaProteccion", false)
+    root(): string {
+        return this.appService.root();
+    }
+
+    @Get('hola')
+    @ReflectMetadata("nesecitaProteccion", true)
+    hola(): string {
+        return 'Hola amigos';
+    }
+}
---
+++ Nest6/src/guards/jwt.guard.ts
@@ -0,0 +1,49 @@
+import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
+import {Observable} from "rxjs/index";
+import {Reflector} from "@nestjs/core";
+import {JwtService} from "../servicios/jwt.service";
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+    constructor(
+        private readonly reflector: Reflector,
+        private readonly _jwtService: JwtService) {
+    }
+
+    canActivate(context: ExecutionContext)
+        : boolean |
+        Promise<boolean> |
+        Observable<boolean> {
+
+
+        const necesitaProteccion = this.reflector.get(
+            "nesecitaProteccion",
+            context.getHandler());
+
+        console.log('necesitaProteccion', necesitaProteccion);
+
+        if (necesitaProteccion) {
+
+            const request = context
+                .switchToHttp()
+                .getRequest();
+
+            const jwt = request.headers.auth;
+            console.log('jwt', jwt);
+            if (jwt) {
+                return this._jwtService
+                    .verificarTokenSync(
+                        jwt
+                    );
+
+            } else {
+                return false;
+            }
+
+        } else {
+            return true
+        }
+
+    }
+
+}
